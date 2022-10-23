558faf28307238c370a5e33fb84501e517019a8b
masveli92 <masveli92@gmail.com>
2022-10-23 11:37:52 +0300

    attempts5

--- src/components/paginationMoviesList/PaginationMoviesList.js
+++ src/components/paginationMoviesList/PaginationMoviesList.js
@@ -1,83 +1,43 @@
 
+import {useSelector} from "react-redux";
 
+import css from './PaginationMoviesList.module.css'
 
+const PaginationMoviesList = ({actualPage, setQuery}) => {
 
+    const {maxPage} = useSelector(state => state.moviesList);
 
+    const firstPage = () => {
+        const page = actualPage = 1
+        setQuery({page: `${page}`})
+    };
 
+    const prevPage = () => {
+        const page = actualPage - 1;
+        setQuery({page: `${page}`})
+    };
 
+    const nextPage = () => {
+        const page = actualPage + 1;
+        setQuery({page: `${page}`})
+    };
 
+    const lastPage = () => {
+        const page = actualPage = 500;
+        setQuery({page: `${page}`})
+    };
 
+    return (
+        <div className={css.pagination}>
 
+                <button onClick={firstPage}>First</button>
+                <button disabled={actualPage<2} onClick={prevPage}>Prew</button>
+                <button > {actualPage}</button>
+                <button disabled={actualPage>499} onClick={nextPage}>next</button>
+                <button onClick={lastPage}>Last</button>
 
+        </div>
+    );
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import {Pagination} from "react-bootstrap";
-//
-// import css from './PaginationMoviesList.module.css'
-// import {useSelector} from "react-redux";
-//
-// const PaginationMoviesList = ({actualPage, setQuery}) => {
-//
-//     const {maxPage} = useSelector(state => state.moviesList);
-//
-//     const prevPage = (num) => {
-//         const page = actualPage - num;
-//         setQuery({page: `${page}`})
-//     };
-//
-//     const nextPage = (num) => {
-//         const page = actualPage + num;
-//         setQuery({page: `${page}`})
-//     };
-//
-//     return (
-//         <div className={css.pagination}>
-//             <Pagination>
-//               {(actualPage - 10 >= 1) && <Pagination.First onClick={() => prevPage(actualPage - 1)}/>}
-//               {(actualPage - 1 >= 1) && <Pagination.Prev onClick={() => prevPage(1)}/>}
-//               {(actualPage - 10 >= 1) && <Pagination.Item onClick={() => prevPage(1)}>{actualPage - 10}</Pagination.Item>}
-//               {(actualPage - 3 >= 1) && <Pagination.Ellipsis disabled={true}/>}
-//
-//                 {(actualPage - 2 >= 1) &&
-//                     <Pagination.Item onClick={() => prevPage(2)}>{actualPage - 1}</Pagination.Item>}
-//                 {(actualPage - 1 >= 1) &&
-//                     <Pagination.Item onClick={() => prevPage(1)}>{actualPage - 1}</Pagination.Item>}
-//
-//                 <Pagination.Item active>{actualPage}</Pagination.Item>
-//
-//                 {(actualPage + 1 <= maxPage) &&
-//                     <Pagination.Item onClick={() => nextPage(1)}>{actualPage + 1}</Pagination.Item>}
-//                 {(actualPage + 2 <= maxPage) &&
-//                     <Pagination.Item onClick={() => nextPage(2)}>{actualPage + 2}</Pagination.Item>}
-//
-//                 {(actualPage + 3 <= maxPage) && <Pagination.Ellipsis disabled={true}/>}
-//                 {(actualPage + 10 <= maxPage) &&
-//                     <Pagination.Item onClick={() => nextPage(10)}>{actualPage + 10}</Pagination.Item>}
-//                 {(actualPage + 1 <= maxPage) && <Pagination.Next onClick={() => nextPage(1)}/>}
-//                 {(actualPage + 3 <= maxPage) && <Pagination.Last onClick={() => nextPage(maxPage - actualPage)}/>}
-//             </Pagination>
-//         </div>
-//     );
-// };
-//
-// export {PaginationMoviesList};
+export {PaginationMoviesList};
